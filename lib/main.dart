b05ed5efa468e3e8cbd63bcf1656d9e74858f1a8
DefPavel <33838515+DefPavel@users.noreply.github.com>
2021-05-09 17:35:34 +0300

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:note_application/services/sharedPref.dart';
+import 'view/home_view.dart';
+import 'data/theme.dart';
+
+import 'values/string.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeData theme = appThemeLight;
+  @override
+  void initState() {
+    super.initState();
+    updateThemeFromSharedPref();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appName,
+      theme: theme,
+      home: MyHomePage(title: titleHome, changeTheme: setTheme),
+    );
+  }
+
+  setTheme(Brightness brightness) {
+    if (brightness == Brightness.dark) {
+      setState(() {
+        theme = appThemeDark;
+      });
+    } else {
+      setState(() {
+        theme = appThemeLight;
+      });
+    }
+  }
+
+  void updateThemeFromSharedPref() async {
+    String themeText = await getThemeFromSharedPref();
+    if (themeText == 'light') {
+      setTheme(Brightness.light);
+    } else {
+      setTheme(Brightness.dark);
+    }
+  }
+}
---
+++ lib/values/string.dart
@@ -0,0 +1,33 @@
+//Хранимые данные в String
+
+final String appName = "Заметки (Custom)";
+
+// home_view
+final String titleHome = "Главная страница";
+final String setTextNote = "Записать заметку";
+final String searchText = "Поиск...";
+final String yourNotes = "Заметки";
+final String flagIsTrue = "Отображать только заметки, помеченные как важные.";
+final String addNote = "Содать новую заметку";
+
+
+//setting view
+final String titleSetting = "Настройки";
+//edit view
+final String textEnter = "Введите текст";
+
+final String textSave = "Сохр.";
+
+final String deleteNote = "Удаление Заметки";
+
+final String deleteDescription = "Эта заметка будет удалена без возможности восстановления.";
+
+final String cancel = "Отмена";
+
+
+
+
+
+final String darkTheme = "Темная тема";
+
+final String lightTheme = "Светлая тема";
